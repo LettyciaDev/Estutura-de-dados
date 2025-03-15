8bed89e1112ec766f42f9f03b3b3457363c8cefd
Lettycia Vitória <163659294+LettyciaDev@users.noreply.github.com>
2025-03-15 16:27:27 +0000

    implementação

--- Treinos/TAD2/vetor.c
+++ Treinos/TAD2/vetor.c
@@ -1,13 +1,23 @@
 #include <stdio.h>
 #include "vetor.h"
 #include <stdlib.h>
+#include <stdbool.h>
 
+// **************** interface privada **************** //
 struct vetor{
     int capacity;
     int size;
     float* data;
 };
 
+/* função "privada" ==> não está disponível para os usuarios/programas,
+os outros arquivos que usam o vertor.h*/
+
+bool isFull(Vetor* v){
+    return v->size == v->capacity;
+}
+
+// **************** implementação da interface pública **************** //
 Vetor* create(int capacity){
     Vetor* v = (Vetor*) malloc(sizeof(Vetor));
     v->size = 0;
@@ -31,6 +41,58 @@ int capacityVetor(Vetor* v){
 }
 
 float atVetor(Vetor* v, int index){
+    if(index < 0 || index > v->size){
+        fprintf(stderr, "Error in 'at'\n");
+        fprintf(stderr, "index [%d] is out bounds: [0,%d]", index, v->size);
+        exit(EXIT_FAILURE);
+    }
+
     return *(v->data + index);
+}
+
+float getVetor(Vetor* v, int index){
+    return *(v->data + index);
+}
+
+void appendVetor(Vetor* v, float val){
+    if(isFull(v)){
+        // fprintf(stderr) serve para imprimir erro
+        fprintf(stderr,"Error in 'append'\n");
+        printf("Vector is full\n");
+        // exit - função para sair
+        exit(EXIT_FAILURE); 
+    }
+
+    *(v->data + v->size) = val;
+    v->size++;
+}
+
+void remove(Vetor* v, int index){
+    if(index < 0 || index >= v->size){
+        fprintf(stderr, "Error in 'at'\n");
+        fprintf(stderr, "index [%d] is out bounds: [0,%d]", index, v->size);
+        exit(EXIT_FAILURE);
+    }
+
+    for(int i = index; i < v->size - 1; i++){
+        *(v->data + i) = *(v->data + i + 1);     
+    }
+
+    v->size--;
+}
+void setVetor(Vetor* v, int index, float val){
+    if(index < 0 || index > v->size){
+        fprintf(stderr, "Error in 'at'\n");
+        fprintf(stderr, "index [%d] is out bounds: [0,%d]", index, v->size);
+        exit(EXIT_FAILURE);
+    }
+
+    *(v->data + index) = val;
+}
+
+void printVetor(Vetor* v){
+    for(int i = 0; i < v->size; i++){
+        printf("%f",*(v->data + i));
+    }
 }
 
--- Treinos/TAD2/vetor.h
+++ Treinos/TAD2/vetor.h
@@ -1,29 +1,24 @@
-#include <stdio.h>
+// ************** 
 
 typedef struct vetor Vetor;
+
 // cria o vetor
 Vetor* create(int capacity);
-
 //  destruir vetor
 void destroy(Vetor* v);
-
 // retorna o tamanho do vetor(número atual de elementos inseridos)
 int sizeVetor(Vetor* v);
-
 // retorna a capacidade do vetor(número máximo de elementos)
 int capacityVetor(Vetor* v);
-
 // retorna o elemento do índice 'index' 
 float atVetor(Vetor* v, int index);
-
 // retorna o elemento do índice
 float getVetor(Vetor* v, int index);
-
 // adiciona o valor 'val' no final do vetor. Informa erro se o vetor estiver cheio
 void appendVetor(Vetor* v, float val);
-
+// remove valor no index
+void remove(Vetor* v, int index);
 // atribui o valor 'val' no índice 'index' do vetor de tipo 'tad'
 void setVetor(Vetor* v, int index, float val);
-
 // imprime todos os elementos do vetor
 void printVetor(Vetor* v);
